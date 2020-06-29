fc82f28bf44988fba10ebc24686940b0077b6ab6
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-29 20:06:54 +0200

    change MaterialApp for GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Rick & Morty App',
       theme: ThemeData(
         primarySwatch: Colors.teal,
