5fd604a679c2ba20f3889321542ba04340e68663
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-29 20:30:35 +0200

    add character_api

---
+++ lib/api/character/character_api.dart
@@ -0,0 +1,45 @@
+import 'package:get/get.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+abstract class CharacterApi {
+  Future<dynamic> fetchCharacters();
+}
+
+class CharacterApiImpl extends CharacterApi {
+  GraphQLClient graphQLClient;
+
+  CharacterApiImpl({graphQLClient})
+      : this.graphQLClient = graphQLClient ?? Get.find();
+
+  String charactersQuery() {
+    return '''
+        query {
+      characters(page: 1) {
+        info {
+          count
+          pages
+          next
+          prev
+        }
+        results {
+          id
+          name
+          status
+          species
+          type
+          gender
+          image
+        }
+      }
+    }
+    ''';
+  }
+
+  @override
+  Future fetchCharacters() async {
+    var result = await graphQLClient.query(
+      QueryOptions(documentNode: gql(charactersQuery())),
+    );
+    return result;
+  }
+}
