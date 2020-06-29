da44c18b780504a00ffd2fb09ac87fa9568f794a
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-29 20:31:05 +0200

    add custom exceptions

---
+++ lib/exception/custom_exception.dart
@@ -0,0 +1,25 @@
+import 'package:lumberdash/lumberdash.dart';
+
+class ResponseException implements Exception {
+  final message;
+  ResponseException([this.message]);
+
+  call() => logError(this.toString());
+
+  String toString() {
+    if (message == null) return "Exception";
+    return "ResponseException: $message";
+  }
+}
+
+class ServerException implements Exception {
+  final message;
+  ServerException([this.message]);
+
+  call() => logError(this.toString());
+
+  String toString() {
+    if (message == null) return "Exception";
+    return "ServerException: $message";
+  }
+}
