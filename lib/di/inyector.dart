c9165cc973b64b756622540a510a67222b19385c
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-29 20:47:37 +0200

    add dependencies to inyector

--- lib/di/inyector.dart
+++ lib/di/inyector.dart
@@ -2,10 +2,12 @@ import 'package:get/get.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:rick_and_morty_app/api/character/character_api.dart';
 import 'package:rick_and_morty_app/api/client/graphql_client.dart';
+import 'package:rick_and_morty_app/controller/character_controller.dart';
 import 'package:rick_and_morty_app/service/character_service.dart';
 
 void configureInyector() {
   Get.lazyPut<GraphQLClient>(() => MyGraphQlClient().client);
   Get.lazyPut<CharacterApi>(() => CharacterApiImpl());
   Get.lazyPut<CharacterService>(() => CharacterServiceImpl());
+  Get.lazyPut<CharacterController>(() => CharacterController());
 }
