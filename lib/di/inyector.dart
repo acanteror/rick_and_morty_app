2f13614f14c36fc2686964f990a249a674ea7b4c
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-03 21:30:36 +0200

    add characterbloc

--- lib/di/inyector.dart
+++ lib/di/inyector.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:rick_and_morty_app/client/graphql_client.dart';
 import 'package:rick_and_morty_app/features/characters/domain/repositories/characters_repository.dart';
-import 'package:rick_and_morty_app/features/characters/domain/use_cases/character_controller.dart';
+import 'package:rick_and_morty_app/features/characters/domain/use_cases/bloc/character_bloc.dart';
 import 'package:rick_and_morty_app/features/characters/infraestructure/data_sources/characters_data_source.dart';
 import 'package:rick_and_morty_app/features/characters/infraestructure/repositories/graphql_character_repository.dart';
 
@@ -10,5 +10,5 @@ void configureInyector() {
   Get.lazyPut<GraphQLClient>(() => MyGraphQlClient().client);
   Get.lazyPut<CharactersDataSource>(() => GraphQLCharactersDataSource());
   Get.lazyPut<CharactersRepository>(() => GraphQLCharacterRepository());
-  Get.lazyPut<CharacterController>(() => CharacterController());
+  Get.lazyPut<CharacterBloc>(() => CharacterBloc());
 }
