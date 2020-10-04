5d03c38047452906c84fa3fbc9bf6ebfcb9f2c8a
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-04 12:36:13 +0200

    rename

--- lib/features/characters/domain/use_cases/bloc/character_bloc.dart
+++ lib/features/characters/domain/use_cases/bloc/character_bloc.dart
@@ -12,10 +12,10 @@ part 'character_event.dart';
 part 'character_state.dart';
 
 class CharactersBloc extends Bloc<CharactersEvent, CharactersState> {
-  final CharactersRepository _characterRepository;
+  final CharactersRepository _charactersRepository;
 
-  CharactersBloc({characterRepository})
-      : _characterRepository = characterRepository ?? Get.find<CharactersRepository>(),
+  CharactersBloc({charactersRepository})
+      : _charactersRepository = charactersRepository ?? Get.find<CharactersRepository>(),
         super(CharacterInitial());
 
   @override
@@ -26,7 +26,7 @@ class CharactersBloc extends Bloc<CharactersEvent, CharactersState> {
       yield CharacterLoading();
 
       try {
-        final Data _response = await _characterRepository.fetchCharacters();
+        final Data _response = await _charactersRepository.fetchCharacters();
         final List<Result> _characters = _response.results;
         yield CharacterLoaded(characters: _characters);
       } catch (e) {
