6ae3398333eefd27c7159655a5b1c807bbb39ccf
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-04 10:54:48 +0200

    add

--- test/characters/infraestructure/models/fakes/data_fake.dart
+++ test/characters/infraestructure/models/fakes/data_fake.dart
@@ -1,1 +1,6 @@
+import 'package:rick_and_morty_app/features/characters/infraestructure/models/data/data_model.dart';
 
+import 'info_fake.dart';
+import 'result_fake.dart';
+
+const Data tData = Data(tInfo, [tResult]);
--- test/characters/infraestructure/models/fakes/result_fake.dart
+++ test/characters/infraestructure/models/fakes/result_fake.dart
@@ -1,1 +1,3 @@
+import 'package:rick_and_morty_app/features/characters/infraestructure/models/result/result_model.dart';
 
+const Result tResult = Result('id', 'name', 'status', 'species', 'type', 'gender', 'image');
