05ff90d7210ecee02f04daf23fefd1c834bef610
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-04 10:54:56 +0200

    change path

--- test/characters/infraestructure/models/fixtures/fixture_reader.dart
+++ test/characters/infraestructure/models/fixtures/fixture_reader.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 String fixture(String name) {
   try {
-    return File('../test/fixtures/$name').readAsStringSync();
+    return File('../test/characters/infraestructure/models/fixtures/$name').readAsStringSync();
   } catch (e) {
-    return File('./test/fixtures/$name').readAsStringSync();
+    return File('./test/characters/infraestructure/models/fixtures/$name').readAsStringSync();
   }
 }
