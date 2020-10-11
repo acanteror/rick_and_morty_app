b9f4c4f99f84061359ff28624380fad4c294cb46
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-11 08:40:21 +0200

    add simple functional test

---
+++ test_driver/app_test.dart
@@ -0,0 +1,38 @@
+// Imports the Flutter Driver API.
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Rick & Morty App', () {
+    final charactersPageInitialMessage = find.byValueKey('initialMessageText');
+    final charactersPageFAB = find.byValueKey('charactersPageFAB');
+    final firstCharacterName = find.text('RICK SANCHEZ');
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    test('starts with initial message', () async {
+      expect(
+        await driver.getText(charactersPageInitialMessage),
+        'Pulse para cargar personajes.',
+      );
+    });
+
+    test('after tapping fab load characters', () async {
+      await driver.tap(charactersPageFAB);
+
+      expect(
+        await driver.getText(firstCharacterName),
+        'RICK SANCHEZ',
+      );
+    });
+  });
+}
