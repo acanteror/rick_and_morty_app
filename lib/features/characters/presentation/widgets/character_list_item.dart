638e19f809dc803ae28bc94cd2d10e820e178f5a
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-11 08:48:15 +0200

    Navigate to detail when tap on list item

--- lib/features/characters/presentation/widgets/character_list_item.dart
+++ lib/features/characters/presentation/widgets/character_list_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:rick_and_morty_app/features/character_detail/presentation/pages/character_detail_page.dart';
 import 'package:rick_and_morty_app/features/characters/infraestructure/models/result.dart';
 
 class CharacterListItem extends StatelessWidget {
@@ -15,12 +17,17 @@ class CharacterListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
-      child: ListTile(
-        key: Key('characterListTile'),
-        leading: Image.network(_character.image),
-        title: Text(_character.name.toUpperCase()),
-        subtitle: Text('Status: ${_character.status}'),
-        trailing: Icon(Icons.arrow_forward_ios),
+      child: GestureDetector(
+        child: ListTile(
+          key: Key('characterListTile'),
+          leading: Image.network(_character.image),
+          title: Text(_character.name.toUpperCase()),
+          subtitle: Text('Status: ${_character.status}'),
+          trailing: Icon(Icons.arrow_forward_ios),
+          onTap: () {
+            Get.to(CharacterDetailPage());
+          },
+        ),
       ),
     );
   }
