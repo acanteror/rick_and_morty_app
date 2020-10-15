4878bd6ac4396036f8c0fbcdc436c0ddcaf9f73c
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-15 20:43:04 +0200

    Format

--- lib/features/characters/infraestructure/repositories/graphql_character_repository.dart
+++ lib/features/characters/infraestructure/repositories/graphql_character_repository.dart
@@ -1,24 +1,38 @@
 import 'package:get/get.dart';
-import 'package:rick_and_morty_app/exception/custom_exception.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:rick_and_morty_app/exception/exception_logger.dart';
 import 'package:rick_and_morty_app/features/characters/domain/repositories/characters_repository.dart';
 import 'package:rick_and_morty_app/features/characters/infraestructure/data_sources/characters_data_source.dart';
 import 'package:rick_and_morty_app/features/characters/infraestructure/models/data.dart';
 
-class GraphQLCharacterRepository extends CharactersRepository {
+class GraphQLCharacterRepository extends CharactersRepository with ExceptionLogger {
   final CharactersDataSource _dataSource;
 
-  GraphQLCharacterRepository({CharactersDataSource dataSource}) : _dataSource = dataSource ?? Get.find();
+  GraphQLCharacterRepository({
+    CharactersDataSource dataSource,
+  }) : _dataSource = dataSource ?? Get.find();
 
   Future<Data> fetchCharacters() async {
-    var _response = await _dataSource.fetchCharacters();
+    try {
+      QueryResult _response = await _dataSource.fetchCharacters();
 
-    if (_response.exception != null) {
-      throw ResponseException(_response.exception);
-    }
+      if (_response.exception != null) {
+        throw exception(
+          ExceptionType.response,
+          _response.exception.toString(),
+        );
+      }
 
-    if (_response.data != null) {
-      return Data.fromMap(_response.data['characters']);
+      if (_response.data != null) {
+        return Data.fromMap(_response.data['characters']);
+      }
+
+      throw exception(
+        ExceptionType.server,
+        'Se ha producido un error al contactar con el servidor',
+      );
+    } catch (e) {
+      throw exception(ExceptionType.graphql, e);
     }
-    throw ServerException('Se ha producido un error al contactar con el servidor');
   }
 }
