a8b8f9b72af819108fb812fe614c692fdbcf1351
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-15 20:42:34 +0200

    Create exception logger mixin

---
+++ lib/exception/exception_logger.dart
@@ -0,0 +1,29 @@
+import 'package:lumberdash/lumberdash.dart';
+import 'package:rick_and_morty_app/exception/custom_exception.dart';
+
+enum ExceptionType {
+  response,
+  graphql,
+  server,
+}
+
+class ExceptionLogger {
+  Exception exception(ExceptionType type, String errorMessage) {
+    Exception _exception;
+    switch (type) {
+      case ExceptionType.graphql:
+        _exception = GraphQLException(errorMessage);
+        break;
+      case ExceptionType.response:
+        _exception = ResponseException(errorMessage);
+        break;
+      case ExceptionType.server:
+        _exception = ServerException(errorMessage);
+        break;
+      default:
+        _exception = Exception(errorMessage);
+    }
+    logError(_exception);
+    return _exception;
+  }
+}
