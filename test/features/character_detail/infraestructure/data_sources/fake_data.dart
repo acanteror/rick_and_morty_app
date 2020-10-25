25a3e8fcd409b730e9f00c1f014a6f6bb79462d5
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-25 13:42:42 +0100

    Add characterDetailDataSource test fake data

---
+++ test/features/character_detail/infraestructure/data_sources/fake_data.dart
@@ -0,0 +1,48 @@
+const String characterDetailResponse = r'''
+  {
+  "data": {
+    "character": {
+      {
+        "id": "id",
+        "name": "name",
+        "status": "status",
+        "species": "species",
+        "type": "type",
+        "gender": "gender",
+        "image": "image",
+        "episode": [
+          {
+            "id": "id",
+            "name": "name",
+            "air_date": "air_date",
+            "episode": "episode"
+          }
+        ]
+      }
+    }
+  }
+}
+  ''';
+
+const String characterDetailData = r'''
+  {
+    "character": {
+        "id": "id",
+        "name": "name",
+        "status": "status",
+        "species": "species",
+        "type": "type",
+        "gender": "gender",
+        "image": "image",
+        "episode": [
+          {
+            "id": "id",
+            "name": "name",
+            "air_date": "air_date",
+            "episode": "episode"
+          }
+        ]
+      }
+    }
+  
+  ''';
