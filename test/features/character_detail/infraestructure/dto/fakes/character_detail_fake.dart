2e348a2244ea3dfa95295748fd24e1dd4860ac9c
Antonio Cantero <acr.cuentas@gmail.com>
2020-10-25 12:32:37 +0100

    Add character_detail and episode fakes

---
+++ test/features/character_detail/infraestructure/dto/fakes/character_detail_fake.dart
@@ -0,0 +1,14 @@
+import 'package:rick_and_morty_app/features/character_detail/infraestructure/dto/character_detail.dart';
+
+import 'episode_fake.dart';
+
+CharacterDetail tCharacterDetail = CharacterDetail(
+  id: 'id',
+  name: 'name',
+  status: 'status',
+  species: 'species',
+  type: 'type',
+  gender: 'gender',
+  image: 'image',
+  episodes: [tEpisode],
+);
---
+++ test/features/character_detail/infraestructure/dto/fakes/episode_fake.dart
@@ -0,0 +1,8 @@
+import 'package:rick_and_morty_app/features/character_detail/infraestructure/dto/episode.dart';
+
+Episode tEpisode = Episode(
+  id: 'id',
+  name: 'name',
+  airDate: 'air_date',
+  episode: 'episode',
+);
